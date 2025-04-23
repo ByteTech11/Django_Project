0794d51645352ad2361c869dd2f0c9066865c595
ByteTech11 <muhammad.ayyaz@bytetech.pk>
2025-04-23 19:42:28 +0500

    Update settings.py
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql',
            'NAME': 'DjangoAppServer',  # Database name
            'USER': 'DjangoAppServer',  # Username
            'PASSWORD': 'ByteTech123!', # Password
            'HOST': 'localhost',        # Assuming you're running PostgreSQL locally
            'PORT': '5432',             # Default PostgreSQL port
        }
    }
    
    Updated it

--- fileuploadproject/settings.py
+++ fileuploadproject/settings.py
@@ -72,12 +72,24 @@ WSGI_APPLICATION = 'fileuploadproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+###DATABASES = {
+###    'default': {
+###        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+##}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoAppServer',  # Database name
+        'USER': 'DjangoAppServer',  # Username
+        'PASSWORD': 'ByteTech123!', # Password
+        'HOST': 'localhost',        # Assuming you're running PostgreSQL locally
+        'PORT': '5432',             # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
